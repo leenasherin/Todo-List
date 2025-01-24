2535f68e5a1430ce8386e94007aba40a1788dfe3
leenasherin <leenasherin145@gmail.com>
2025-01-24 10:23:18 +0400

    Todo Completed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css'
 const App = () => {
   const [tasks, setTasks] = useState([]);
 
-  // Fetch tasks from the backend
+  
   const fetchTasks = async () => {
     const response = await axios.get("http://localhost:5000/tasks");
     setTasks(response.data);
@@ -17,19 +17,19 @@ const App = () => {
     fetchTasks();
   }, []);
 
-  // Add a new task
+  
   const addTask = async (newTask) => {
     const response = await axios.post("http://localhost:5000/tasks", newTask);
     setTasks([...tasks, response.data]);
   };
 
-  // Update an existing task
+  
   const updateTask = async (id, updatedTask) => {
     await axios.put(`http://localhost:5000/tasks/${id}`, updatedTask);
     fetchTasks();
   };
 
-  // Delete a task
+ 
   const deleteTask = async (id) => {
     await axios.delete(`http://localhost:5000/tasks/${id}`);
     fetchTasks();
